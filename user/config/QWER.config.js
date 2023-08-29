a7b8a77dcedb8718952d83424765753c7c85c2d9
MotiCAT <youyuzhangye@gmail.com>
2023-08-29 12:49:36 +0900

    Update QWER.config.js

--- user/config/QWER.config.js
+++ user/config/QWER.config.js
@@ -2,7 +2,7 @@ export const UserConfig = {
   DefaultDesktopShowTagFilter: false,
   DefaultCoverStyle: 'IN',
   DefaultTagName: 'tags',
-  DefaultPostLanguage: 'English',
+  DefaultPostLanguage: 'Japanese',
   PostLanguageTagName: 'Language',
   SeriesTagName: 'Series',
   YearTagName: 'Year',
